6d93fefe522d46db5e129a4c21be4d9b6cc97bad
May Wu <maychangwu@gmail.com>
2022-03-17 22:59:00 -0400

    github login authentication done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 // import './css/Home.css';
+import {signInWithGitHub} from '../db/Firebase'
 
 function Home() {
   return (
     <div className='Home'>
       <header className='Home-header'>
         <p>Homepage</p>
+        <button onClick={signInWithGitHub}>Sign in with Github</button>
       </header>
     </div>
   );

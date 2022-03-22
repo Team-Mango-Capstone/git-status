5bae66a12ed09458c058c6cf48c52784a3578eb8
Diana Viglucci <dvigl86@gmail.com>
2022-03-22 15:26:48 -0400

    break before code review

--- src/components/homeCards/MostProductive.js
+++ src/components/homeCards/MostProductive.js
@@ -8,21 +8,21 @@ export function MostProductive(props) {
   
   const repoArr = props.userRepos.items || [];
 
-  const addArrs = (arrOfArrs) => {
-    const finalArr = [];
+  // const addArrs = (arrOfArrs) => {
+  //   const finalArr = [];
     
-    for (let i = 0; i < arrOfArrs[0].length; i++) {
-      finalArr.push(0)
-    };
+  //   for (let i = 0; i < arrOfArrs[0].length; i++) {
+  //     finalArr.push(0)
+  //   };
       
-    for (let i = 0; i < arrOfArrs.length; i++) {
-      for (let j = 0; j < finalArr.length; j++) {
-        finalArr[j] = finalArr[j] + arrOfArrs[i][j];
-      };
-    };
+  //   for (let i = 0; i < arrOfArrs.length; i++) {
+  //     for (let j = 0; j < finalArr.length; j++) {
+  //       finalArr[j] = finalArr[j] + arrOfArrs[i][j];
+  //     };
+  //   };
     
-    console.log('FINAL ARR >>>>>>', finalArr);
-  };
+  //   console.log('FINAL ARR >>>>>>', finalArr);
+  // };
 
   useEffect(() => {
     const calculateMostProductive = async () => {
@@ -37,7 +37,34 @@ export function MostProductive(props) {
       });
       return allResponses;
     };
-    addArrs(calculateMostProductive());
+    // calculateMostProductive();
+    //let arrOfWeeklyCommits = calculateMostProductive();
+    // console.log('arrOfWeeklyCommits >>>>', arrOfWeeklyCommits)
+    // let consolidatedArr = addArrs(arrOfWeeklyCommits);
+    // console.log('consolidatedArr >>>>', consolidatedArr)
+    const addArrs = async (arrOfArrs) => {
+      const myVar =  await arrOfArrs
+      console.log('myvar >>>', myVar)
+      const finalArr = [];
+      
+      for (let i = 0; i < 53; i++) {
+        finalArr.push(0)
+      };
+        
+      for (let i = 0; i < myVar.length; i++) {
+        for (let j = 0; j < finalArr.length; j++) {
+          // finalArr[j] = finalArr[j] + myVar[i][j];
+          finalArr[j] += myVar[i][j];
+        };
+      };
+      
+      console.log('FINAL ARR >>>>>>', finalArr);
+    };
+    let arrAllWeeklyCommits = addArrs(calculateMostProductive());
+    // find largest number in array, store in variable
+    // find index of largest number, convert to week number
+    // convert week number to speciic dates of the year
+    // print out dates and largest number
   }, [props.userRepos.items]);
 
   return (

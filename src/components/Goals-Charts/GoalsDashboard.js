d40063a0519eb02aa191aa047497a0e2f2f686f8
May Wu <maychangwu@gmail.com>
2022-03-21 23:53:56 -0400

    added goal charts

---
+++ src/components/Goals-Charts/GoalsDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DonutChart from './DonutChart';
+import BarChart from './BarChart';
+
+export const GoalsDashboard = () => {
+  return (
+    <div>GoalsDashboard
+   <DonutChart />
+   <BarChart />
+    </div>
+  )
+}
--- src/components/Insights.js
+++ src/components/Insights.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { GoalsDashboard } from './Goals-Charts/GoalsDashboard';
 
 function Insights() {
-  return <div className='insights'>insights bar</div>;
+  return (
+  <div className='insights'>insights bar
+  
+  <GoalsDashboard/>
+  </div>
+  );
 }
 
 export default Insights;

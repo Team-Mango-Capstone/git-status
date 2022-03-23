c67775703a6dc0a361577873646fe09ff315d5e1
May Wu <maychangwu@gmail.com>
2022-03-23 00:35:09 -0400

    fixed css bug

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './css/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Popper from '@popperjs/core';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Popper from '@popperjs/core';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(

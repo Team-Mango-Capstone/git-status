171318569c0f6f3e160f9a5296a52d6f0472421c
May Wu <maychangwu@gmail.com>
2022-03-23 00:13:48 -0400

    updated goal dashboard

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './css/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Popper from '@popperjs/core';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>

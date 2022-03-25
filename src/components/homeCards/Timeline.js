4fd441a4a483d01effbdcd899bbbd3067ee1ff8d
Diana Viglucci <dvigl86@gmail.com>
2022-03-25 09:00:14 -0400

    can convert raw data into format needed for chart

--- src/components/homeCards/Timeline.js
+++ src/components/homeCards/Timeline.js
@@ -4,8 +4,8 @@ import { useQuery, gql } from "@apollo/client";
 const screenName = localStorage.getItem("screenName");
 
 const CONTRIBUTIONS_QUERY = gql`
-query($userName:String!) { 
-    user(login: $userName){
+  query ($userName: String!) {
+    user(login: $userName) {
       contributionsCollection {
         contributionCalendar {
           totalContributions
@@ -18,41 +18,26 @@ query($userName:String!) {
         }
       }
     }
-}
+  }
 `;
 
 export default function Timeline() {
   const { data, loading, error } = useQuery(CONTRIBUTIONS_QUERY, {
-      variables: { "userName": screenName},
+    variables: { userName: screenName },
   });
 
   if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>
+  if (error) return <pre>{error.message}</pre>;
 
   const useableData = data.user.contributionsCollection.contributionCalendar;
-  const chartData = data.user.contributionsCollection.contributionCalendar.weeks;
+  const chartData =
+    data.user.contributionsCollection.contributionCalendar.weeks;
 
   const testData = chartData.slice(50);
 
-  /*
-  using chartData:
-  1) map over the initial array, which is all the weeks
-  2) map over each individual week:
-    a. add number of total contributions that week
-    b. store the date of the first day of that week (aka index 0)
-  3) put data into below format
-
-  data needs to end up as an array of objects in this format:
-  {
-    x: "2022-08-22", // date
-    y: 0, // must be 0!!!
-    r: 10, // # of contributions
-  },
-  */
-
-  console.log('DATA', data)
-
-  const convertData = (arr) => {
+  // returns an array of arrays in the below format:
+  // [date, number of contributions for the week starting on that date]
+  const compileRawData = (arr) => {
     let finalElement = [];
 
     for (let i = 0; i < arr.length; i++) {
@@ -64,25 +49,36 @@ export default function Timeline() {
         total += dayCount;
       }
       finalElement[i].push(total);
+    }
+    return finalElement;
+  };
+  
+  // returns an array of objects in the format needed for Chart.js:
+  // {
+  //   x: "YYYY-MM-DD",
+  //   y: 0,
+  //   r: # of contributions
+  // },
+  const prepDataForChart = (arr) => {
+    const finalArr = arr.map((element) => {
+      console.log("element >>>", element);
+      return {
+        x: element[0],
+        y: 0,
+        r: element[1],
+      };
+    });
 
-    //   let allDaysInfo = currentElem.contributionDays.map((dayElement) => {
-    //     if (currentElem.contributionDays.indexOf(dayElement) === 0) {
-    //       daysArr.push(dayElement.date);
-    //     }
-    //     return daysArr;
-    //   })
-    //   finalElement.push(allDaysInfo);
-    // }
-  }
-  return finalElement;
-}
+    return finalArr;
+  };
 
-  console.log('ANSWER >>>>', convertData(testData));
+  console.log("ANSWER >>>>", prepDataForChart(compileRawData(chartData)));
 
   return (
     <div>
-        You have made {useableData.totalContributions} contributions in the past year.
-        <canvas id="myChart"></canvas>
+      You have made {useableData.totalContributions} contributions in the past
+      year.
+      <canvas id="myChart"></canvas>
     </div>
   );
 }

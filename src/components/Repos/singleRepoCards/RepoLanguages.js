ebddd4cb582c18711202028b49487f04a05cd7bc
choi2010 <lindachoi05@gmail.com>
2022-03-29 14:52:47 -0400

    Added in horizontal bar chart

--- src/components/Repos/singleRepoCards/RepoLanguages.js
+++ src/components/Repos/singleRepoCards/RepoLanguages.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import DonutChart from './SingleRepoDonut.js'
+import HorizontalBarChart from './SingleRepoHorizontal'
+
 
 
 function RepoLanguages(props) {
   return (
     <div className='single-repo-languages'>
       <h2>RepoLanguages</h2>
-      <div style={{ width: 250 }}>
+      <div style={{ width: 750 }}>
         {/* language chart */}
-        <DonutChart repoLang={props.repoLang} />
+        {/* <DonutChart repoLang={props.repoLang} /> */}
+        <HorizontalBarChart repoLang={props.repoLang} />
+
+
       </div>
     </div>
   );
---
+++ src/components/Repos/singleRepoCards/SingleRepoHorizontal.js
@@ -0,0 +1,111 @@
+// SingleRepo Donut File 
+import React, { useContext } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+// import '../../../css/DonutChart.css';
+import Chart from 'react-apexcharts';
+
+ChartJS.register(ArcElement); //Tooltip removed
+ChartJS.register(ChartDataLabels);
+// ChartJS.defaults.color = '#fff';
+ChartJS.defaults.defaultFont = 'Georgia';
+
+const dummyData = {
+    'CSS': 959,
+    "HTML": 221,
+    "JavaScript": 64845
+}
+function languageLabels(obj) {
+    let dataArr = [];
+    let langArr = [];
+    let valueArr = [];
+    for (let language in obj) {
+        langArr.push(language);
+        dataArr.push(obj[language]);
+    }
+    dataArr = [langArr, dataArr];
+    return dataArr;
+}
+
+for (let language in dummyData) {
+    console.log("language", language);
+}
+
+const options = {
+    plugins: {
+        title: {
+            font: {
+                size: 20,
+            },
+            padding: {
+                top: 30,
+                bottom: 30,
+            },
+            responsive: true,
+            animation: {
+                animateScale: false,
+            },
+        },
+    },
+
+    tooltip: { enabled: false },
+    plugins: [ChartDataLabels],
+    plugins: {
+        datalabels: {
+            formatter: (value, ctx) => {
+                let sum = 0;
+                let dataArr = ctx.chart.data.datasets[0].data;
+                dataArr.map(data => {
+                    sum += data;
+                });
+                let percentage = (value * 100 / sum).toFixed(0) + "%";
+                return percentage;
+            },
+        },
+        legend: {
+            display: false
+        },
+        tooltip: {
+            enabled: false
+        }
+    },
+    indexAxis: 'y',
+    scales: {
+        x: {
+            ticks: {
+                display: false,
+            }
+        },
+        y: {
+            ticks: {
+                display: true,
+            }
+        }
+    }
+};
+export default function horizontalBarChart(props) {
+    console.log("This is the props inside the Horizontal Bar Chart", props)
+
+    const data = {
+        labels: languageLabels(props.repoLang)[0],
+        datasets: [
+            {
+                label: 'Languages in Repo',
+                data: languageLabels(props.repoLang)[1],
+                // borderColor: ['rgba(255,206,86,0.2)'],
+                backgroundColor: ["#0074D9", "#FF4136", "#2ECC40"],
+                // barThickness: 80,
+
+                // backgroundColor: ['563d7c', '#fle05a', '#e34c26'],
+                // pointBackgroundColor: 'rgba(255,206,86,0.2)',
+            },
+        ],
+    };
+
+    return (
+        <div className='horizontal-bar-chart'>
+            <Bar data={data} options={options} />
+        </div>
+    );
+}

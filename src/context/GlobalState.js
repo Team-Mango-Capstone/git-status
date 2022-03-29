74c1ffa256f8bfb15be91dd9c56997e55291cfd9
May Wu <maychangwu@gmail.com>
2022-03-28 21:08:59 -0400

    context fixed

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -11,25 +11,13 @@ export const GlobalProvider = (props) => {
     const [currentGoals, setCurrentGoals] = useState([]);
     const [completedGoals, setCompletedGoals] = useState([]);
     const [tasks, setTasks] = useState([]);
+    const [userLanguages, setUserLanguages] = useState({});
 
     const githubUsername = localStorage.getItem('screenName');
     const uid = window.localStorage.getItem('uid');
 
     useEffect(() => {
-      if(uid){
-        const makeRequest = async () => {
-         
-              const userData = await axios.get(
-                `https://api.github.com/users/${githubUsername}`
-              );
-              setUserData(userData.data);
-      
-              const userRepos = await axios.get(
-                `https://api.github.com/search/repositories?q=user:${githubUsername}+fork:true+archived:false&per_page=100`
-              );
-              setUserRepos(userRepos.data);
-             
-          };
+          if(uid){
           //fetch firebase data
             const currentGoalsQuery = query(
               collection(db, 'allUsers', uid, 'userGoals'),
@@ -75,50 +63,71 @@ export const GlobalProvider = (props) => {
               fetchCurrentGoals();
               fetchCompletedGoals();
               fetchTasks();
-              makeRequest();
             };
           }
           
     }, []); 
+
+
+    useEffect(() => {
+      const makeRequest = async () => {
+          try {
+            const userData = await axios.get(
+              `https://api.github.com/users/${githubUsername}`
+            );
+            setUserData(userData.data);
+    
+            const userRepos = await axios.get(
+              `https://api.github.com/search/repositories?q=user:${githubUsername}+fork:true&per_page=100`
+            );
+            setUserRepos(userRepos.data);
+
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        makeRequest();
+  }, []); //we pass state since we're accessing it
   
-    // const [userLanguages, setUserLanguages] = useState({});
-    // const repoArr = userRepos.items || [];
 
-    // useEffect(() => {
-    //   const calculateLanguages = async () => {
-    //     repoArr.map(async (repo) => {
-    //       try {
-    //         const { data } = await axios.get(
-    //           `https://api.github.com/repos/${githubUsername}/${repo.name}/languages`
-    //         );
-    //         for (const language in data) {
-    //           let newObj = {};
-    //           //if language is already in our userLanguages piece of state, increment it
-    //           if (Object.keys(userLanguages).includes(language)) {
-    //             newObj[language] = userLanguages[language] + data[language];
-    //             let updatedLangs = Object.assign(userLanguages, newObj);
-    //             setUserLanguages(updatedLangs);
-    //           }
-    //           // otherwise, add that language to userLanguages
-    //           else {
-    //             newObj[language] = data[language];
-    //             let updatedLangs = Object.assign(userLanguages, newObj);
-    //             setUserLanguages(updatedLangs);
-    //           }
-    //         }
-    //       } catch (error) {
-    //         console.log(error);
-    //       }
-    //     });
-    //   };
-    //   calculateLanguages();
+    const repoArr = userRepos.items || [];
 
-    // }, [userRepos.items]);
-    console.log(userRepos)
+    useEffect(() => {
+      const calculateLanguages = async () => {
+        repoArr.map(async (repo) => {
+          try {
+            const { data } = await axios.get(
+              `https://api.github.com/repos/${githubUsername}/${repo.name}/languages`
+            );
+            for (const language in data) {
+              let newObj = {};
+              //if language is already in our userLanguages piece of state, increment it
+              if (Object.keys(userLanguages).includes(language)) {
+                newObj[language] = userLanguages[language] + data[language];
+                let updatedLangs = Object.assign(userLanguages, newObj);
+                setUserLanguages(updatedLangs);
+              }
+              // otherwise, add that language to userLanguages
+              else {
+                newObj[language] = data[language];
+                let updatedLangs = Object.assign(userLanguages, newObj);
+                setUserLanguages(updatedLangs);
+              }
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        });
+      };
+      calculateLanguages();
+
+    }, [userRepos.items]);
+
+    // console.log(userRepos)
 
     return (
       <GlobalContext.Provider
-        value={{userRepos, userData, currentGoals, completedGoals, tasks}}
+        value={{userRepos, userData, currentGoals, completedGoals, tasks, userLanguages}}
       >
         {props.children}
       </GlobalContext.Provider>
